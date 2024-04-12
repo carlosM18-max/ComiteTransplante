17dde28c9029e8870ace9afafc7b6889dbbba790
Sebas-ctrl <210505@utxicotepec.edu.mx>
2024-04-12 01:05:11 -0600

    [Sebas] Validaciones de campos obligatorios

--- Frontend/src/views/FormWizard/SimpleWizard.vue
+++ Frontend/src/views/FormWizard/SimpleWizard.vue
@@ -1,6 +1,13 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
   <b-row>
+    <!-- ALERTAS -->
+    <b-alert :show="mostrarAlerta" variant=" " dismissible fade class="text-white bg-warning">
+      <div class="iq-alert-icon">
+        <i class="ri-alert-fill"></i>
+      </div>
+      <div class="iq-alert-text">¡El campo de <b>donatario</b> no puede ir vacío!</div>
+    </b-alert>
     <b-col sm="12">
       <iq-card no-body>
         <template v-slot:headerTitle>
@@ -10,25 +17,25 @@
         <template v-slot:body>
           <div class="stepwizard mt-4">
             <ul class="stepwizard-row setup-panel">
-              <div id="user" class="wizard-step active"
+              <div id="donatario" class="wizard-step active"
                 :class="`${currentindex == 1 ? 'active' : ''} ${currentindex > 1 ? 'done active' : ''}`">
-                <a href="#user-detail" class="active btn"> <i class="ri-user-fill text-primary"></i><span>Detalles del
+                <a href="" class="active btn"> <i class="ri-user-fill text-primary"></i><span>Detalles del
                     donatario</span> </a>
               </div>
-              <div id="document" class="wizard-step"
+              <div id="especificaciones" class="wizard-step"
                 :class="`${currentindex == 2 ? 'active' : ''} ${currentindex > 2 ? 'done active' : ''}`">
-                <a href="#user-detail" class="btn btn-default disabled active"> <i
-                    class="ri-user-fill text-danger"></i><span>Especificaciones de la donacion</span> </a>
+                <a href="" class="btn btn-default disabled active"> <i
+                    class="ri-user-fill text-danger"></i><span>Especificaciones de la donación</span> </a>
               </div>
-              <div id="bank" class="wizard-step"
+              <div id="resumen" class="wizard-step"
                 :class="`${currentindex == 3 ? 'active' : ''} ${currentindex > 3 ? 'done active' : ''}`">
-                <a href="#user-detail" class="btn btn-default disabled active"> <i
-                    class="ri-user-fill text-success"></i><span>Datos del medico</span> </a>
+                <a href="" class="btn btn-default disabled active"> <i
+                    class="ri-user-fill text-success"></i><span>Resumen</span> </a>
               </div>
-              <div id="confirm" class="wizard-step"
+              <div id="confirmacion" class="wizard-step"
                 :class="`${currentindex == 4 ? 'active' : ''} ${currentindex > 4 ? 'done active' : ''}`">
-                <a href="#cpnfirm-data" class="btn btn-default disabled active"> <i
-                    class="ri-check-fill text-warning"></i><span>Confirmacion</span> </a>
+                <a href="" class="btn btn-default disabled active"> <i
+                    class="ri-check-fill text-warning"></i><span>Confirmación</span> </a>
               </div>
             </ul>
           </div>
@@ -47,78 +54,17 @@
                     <b-col md="6">
                       <b-form-group label-for="donatario_select" label="CURP: *">
                         <b-form-select plain v-model="solicitud.donatario" :options="llenarPersonasSelect()"
-                          id="donatario_select">
+                          id="donatario_select" :class="{ 'is-invalid': !solicitud.donatario }">
                           <template v-slot:first>
                             <b-form-select-option :value="null" disabled>Selecciona una CURP</b-form-select-option>
                           </template>
                         </b-form-select>
-                        <div class="invalid-feedback">
-                          <span>{{ errors.donatario }}</span>
-                        </div>
+                        <div class="invalid-feedback">¡La CURP del donatario es obligatoria!</div>
                       </b-form-group>
                     </b-col>
-                    <!-- <b-col md="6">
-                      <b-form-group label="Primer apellido: *">
-                        <Field v-model="solicitud.primerApellido" type="text" class="form-control" name="LastName"
-                          placeholder="Enter Last Name" :class="{ 'is-invalid': errors.LastName }" />
-                        <div class="invalid-feedback">
-                          <span>{{ errors.LastName }}</span>
-                        </div>
-                      </b-form-group>
-                    </b-col>
-                    <b-col md="6">
-                      <b-form-group label="Segundo apellido: *">
-                        <Field v-model="solicitud.segundoApellido" type="text" class="form-control"
-                          name="SecondLastName" placeholder="Enter Second Last Name"
-                          :class="{ 'is-invalid': errors.SecondLastName }" />
-                        <div class="invalid-feedback">
-                          <span>{{ errors.SecondLastName }}</span>
-                        </div>
-                      </b-form-group>
-                    </b-col>
-
-                    <b-col md="6">
-                      <b-form-group label="Curp: *">
-                        <Field class="form-control" v-model="solicitud.curp" type="text" placeholder="CURP" name="CURP"
-                          :class="{ 'is-invalid': errors.CURP }"></Field>
-                        <div class="invalid-feedback">
-                          <span>{{ errors.CURP }}</span>
-                        </div>
-                      </b-form-group>
-                    </b-col>
-                    <b-col md="6">
-                      <b-form-group label="NSS: *">
-                        <Field type="text" class="form-control" v-model="solicitud.nss" name="NSS"
-                          :class="{ 'is-invalid': errors.NSS }" placeholder="NSS" />
-                        <div class="invalid-feedback">
-                          <span>{{ errors.NSS }}</span>
-                        </div>
-                      </b-form-group>
-                    </b-col>
-                    <b-col md="6">
-                      <b-form-group label="Grupo y tipo de sangre: *">
-                        <b-form-select v-model="solicitud.grupoTipoSangre" :options="bloodGroupOptions"
-                          :class="{ 'is-invalid': errors.bloodGroup }" />
-                        <div class="invalid-feedback">
-                          <span>{{ errors.bloodGroup }}</span>
-                        </div>
-                      </b-form-group>
-                    </b-col>
-                    <b-col md="6">
-                      <b-form-group label="Estado: *">
-                        <b-form-select v-model="solicitud.estado" :options="stateOptions"
-                          :class="{ 'is-invalid': errors.estado }" />
-                        <div class="invalid-feedback">
-                          <span>{{ errors.State }}</span>
-                        </div>
-                      </b-form-group>
-                    </b-col> -->
-
-                  </b-row>
-                  <b-row>
                   </b-row>
                 </div>
-                <a href="#personal" class="btn btn-primary next action-button float-end" @click="changeTab(2)"
+                <a href="#especificaciones" class="btn btn-primary next action-button float-end" @click="changeTab(2)"
                   value="Next">Siguiente</a>
               </fieldset>
             </div>
@@ -135,81 +81,62 @@
                     <b-col md="6">
                       <b-form-group label-for="donador_select" label="CURP del donador: *">
                         <b-form-select plain v-model="solicitud.donador" :options="llenarPersonasSelect()"
-                          id="donador_select">
+                          id="donador_select" :class="{ 'is-invalid': !solicitud.donador }">
                           <template v-slot:first>
                             <b-form-select-option :value="null" disabled>Selecciona una CURP</b-form-select-option>
                           </template>
                         </b-form-select>
-                        <div class="invalid-feedback">
-                          <span>{{ errors.donador }}</span>
-                        </div>
+                        <div class="invalid-feedback">¡La CURP del donador es obligatoria!</div>
                       </b-form-group>
                     </b-col>
                     <b-col md="6">
                       <b-form-group label-for="medico_select" label="CURP del médico: *">
                         <b-form-select plain v-model="solicitud.medico" :options="llenarMedicoSelect()"
-                          id="medico_select">
+                          id="medico_select" :class="{ 'is-invalid': !solicitud.medico }">
                           <template v-slot:first>
                             <b-form-select-option :value="null" disabled>Selecciona una CURP</b-form-select-option>
                           </template>
                         </b-form-select>
-                        <div class="invalid-feedback">
-                          <span>{{ errors.medico }}</span>
-                        </div>
+                        <div class="invalid-feedback">¡La CURP del médico es obligatoria!</div>
                       </b-form-group>
                     </b-col>
                     <b-col md="6">
                       <b-form-group label="Tipo de Órgano: *">
                         <b-form-select v-model="solicitud.organo" :options="llenarOrganosSelect()"
-                          :class="{ 'is-invalid': errors.organo }" />
-                        <div class="invalid-feedback">
-                          <span>{{ errors.organo }}</span>
-                        </div>
+                          :class="{ 'is-invalid': !solicitud.organo }" />
+                        <div class="invalid-feedback">¡El órgano es obligatorio!</div>
                       </b-form-group>
                     </b-col>
                     <b-col md="6">
                       <b-form-group label="Prioridad: *">
                         <b-form-select v-model="solicitud.prioridad" :options="opcionesPrioridad"
-                          :class="{ 'is-invalid': errors.prioridad }" />
-                        <div class="invalid-feedback">
-                          <span>{{ errors.prioridad }}</span>
-                        </div>
+                          :class="{ 'is-invalid': !solicitud.prioridad }"/>
+                        <div class="invalid-feedback">¡La prioridad es obligatoria!</div>
                       </b-form-group>
                     </b-col>
                     <b-col md="6">
-                      <b-form-group label="Fecha de Donación: *">
-                        <Field v-model="solicitud.fecha_solicitud" type="date" class="form-control" name="fecha_solicitud"
-                          :class="{ 'is-invalid': errors.fecha_solicitud }" />
-                        <div class="invalid-feedback">
-                          <span>{{ errors.fecha_solicitud }}</span>
-                        </div>
+                      <b-form-group label="Fecha de solicitud: *">
+                        <Field v-model="solicitud.fecha_solicitud" type="date" class="form-control"
+                          name="fecha_solicitud" :class="{ 'is-invalid': !solicitud.fecha_solicitud }" />
+                        <div class="invalid-feedback">¡La fecha de solicitud es obligatoria!</div>
                       </b-form-group>
                     </b-col>
-                    <!-- <b-col md="6">
-                      <b-form-group label="Estado: *">
-                        <b-form-select v-model="solicitud.transplant_Status" :options="transplantStatusOptions"
-                          :class="{ 'is-invalid': errors.transplant_Status }" />
-                        <div class="invalid-feedback">
-                          <span>{{ errors.Transplant_Status }}</span>
-                        </div>
-                      </b-form-group>
-                    </b-col> -->
                   </b-row>
                 </div>
-                <a href="#payment" @click="changeTab(3)" class="btn btn-primary next action-button float-end"
+                <a href="#resumen" @click="changeTab(3)" class="btn btn-primary next action-button float-end"
                   value="Next">Siguiente</a>
-                <a href="#account" @click="changeTab(1)"
+                <a href="#donatario" @click="changeTab(1)"
                   class="btn btn-dark previous action-button-previous float-end me-1" value="Previous">Anterior</a>
               </fieldset>
             </div>
 
             <!-- Parte 3 informacion del medico -->
-            <div id="payment" :class="`${currentindex == 3 ? 'show' : 'd-none'}`">
+            <div id="resumen" :class="`${currentindex == 3 ? 'show' : 'd-none'}`">
               <fieldset>
                 <div class="form-card text-start">
                   <b-row>
                     <div class="col-7">
-                      <h3 class="mb-4">Detalles del medico:</h3>
+                      <h3 class="mb-4">Resumen:</h3>
                     </div>
                   </b-row>
                   <b-row>
@@ -326,7 +253,7 @@ export default {
   },
   data() {
     // Define el esquema de validación
-    const schema = yup.object({
+    const schema = yup.object().shape({
       donatario: yup.string().required(),
       donador: yup.string().required(),
       medico: yup.string().required(),
@@ -343,45 +270,58 @@ export default {
         organo: '',
         prioridad: '',
         fecha_solicitud: '',
-        dias_espera: '',
       },
       obtenerPersonasInstancia,
       obtenerPersonalMedicoInstancia,
       currentindex: 1,
       schema,
+      mostrarAlerta: false,
       obtenerOrganosInstancia,
       opcionesPrioridad: ['Urgente', 'Alta', 'Moderada'],
+      errors: {}
     }
   },
   methods: {
-    llenarOrganosSelect(){
+    llenarOrganosSelect() {
       let organos = []
       obtenerOrganosInstancia.forEach(organo => {
-        organos.push({value: organo.ID, text: organo.nombre})
+        organos.push({ value: organo.ID, text: organo.nombre })
       })
-      
+
       return organos
     },
-    llenarPersonasSelect(){
+    llenarPersonasSelect() {
       let personas = []
       obtenerPersonasInstancia.forEach(persona => {
-        personas.push({value: persona.ID, text: persona.curp})
+        personas.push({ value: persona.ID, text: persona.curp })
       })
-      
+
       return personas
     },
-    llenarMedicoSelect(){
+    llenarMedicoSelect() {
       let medicos = []
-      
+
       obtenerPersonasInstancia.forEach(persona => {
-        if(obtenerPersonalMedicoInstancia.some(medico => medico.persona_ID === persona.ID)){
-          medicos.push({value: persona.ID, text: persona.curp})
+        if (obtenerPersonalMedicoInstancia.some(medico => medico.persona_ID === persona.ID)) {
+          medicos.push({ value: persona.ID, text: persona.curp })
         }
       })
       return medicos
     },
     changeTab(val) {
       this.currentindex = val;
+      if (val === 2) {
+        if (this.solicitud.donatario == '') {
+          this.currentindex = 1
+          return
+        }
+      }
+      if (val === 3) {
+        if (this.solicitud.donador == '' || this.solicitud.medico == '' || this.solicitud.organo == '' || this.solicitud.prioridad == '' || this.solicitud.fecha_solicitud == '') {
+          this.currentindex = 2
+          return
+        }
+      }
       // Verifica si el último paso del wizard se ha completado
       if (val === 4) {
         // Realiza el envío de los datos al backend
